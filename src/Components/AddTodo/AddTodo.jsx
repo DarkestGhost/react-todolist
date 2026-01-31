0d9fb6eb4e4a7de6c4e2e270c2f259ac847cb98c
Ali Aghaziarati <alihp6120@gmail.com>
2026-01-31 11:35:54 +0330

    first commit

--- src/Components/AddTodo/AddTodo.jsx
+++ src/Components/AddTodo/AddTodo.jsx
@@ -24,12 +24,12 @@ const AddTodo = ({ onAddTodo }) => {
         ref={inputRef}
         type="text"
         placeholder="Add a new task..."
-        className="w-full sm:w-4/5 p-3 placeholder-gray-400 bg-white/80 text-gray-900 dark:bg-gray-700 dark:text-gray-100 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:bg-white dark:focus:bg-gray-900 transition-all ease-linear rounded-xl shadow-md"
+        className="w-full sm:w-4/5 p-3 placeholder-gray-400 text-gray-900 bg-white/80 dark:bg-gray-700 dark:text-gray-100 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:bg-white dark:focus:bg-gray-900 transition-all ease-linear rounded-xl shadow-md"
       />
       <button
         type="submit"
         onClick={() => handleChangeAddTodoInput()}
-        className="w-full sm:w-auto px-6 py-3 bg-blue-500 text-white hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-600 focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-400 transition-colors duration-300 ease-linear font-semibold shadow-md cursor-pointer rounded-xl"
+        className="w-full sm:w-auto px-6 py-3 text-white bg-blue-500 hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-600 focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-400 transition-colors duration-300 ease-linear font-semibold shadow-md cursor-pointer rounded-xl"
       >
         Add
       </button>
